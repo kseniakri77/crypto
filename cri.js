c7f481dd70243c84e94bea8321c86ba7a5c27d59
kseniakri77 <102622991+kseniakri77@users.noreply.github.com>
2022-03-29 22:19:43 +0300

    Create cri.js

---
+++ cri.js
@@ -0,0 +1,7 @@
+echo "# crypto" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kseniakri77/crypto.git
+git push -u origin main
